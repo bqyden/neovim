bff4992490dbf8fc0962b57715a7f52a8f7ae35d
Bqyden <bqydenwinter@gmail.com>
2024-09-01 21:47:07 -0400

    Added neo-tree auto-refresh

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,6 +10,7 @@ return {
   config = function()
     require("neo-tree").setup({
       filesystem = {
+        use_libuv_file_watcher = true,
         filtered_items = {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
           hide_dotfiles = false,
@@ -17,6 +18,10 @@ return {
         }
       },
     })
+
+    require("neo-tree.sources.filesystem.commands").refresh(
+      require("neo-tree.sources.manager").get_state("filesystem")
+    )
 
     vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {}) -- toggle neo-tree
   end
