6cba2dfed6c8999758bf2a03647f3b728056eaa5
Bqyden <bqydenwinter@gmail.com>
2024-09-01 23:56:50 -0400

    added lazygit pluginn

--- lua/plugins/git-integrations.lua
+++ lua/plugins/git-integrations.lua
@@ -9,4 +9,16 @@ return {
   {
     'tpope/vim-fugitive',
   },
+  {
+    'kdheepak/lazygit.nvim',
+    dependencies =  {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function ()
+      require("telescope").load_extension("lazygit")
+
+      vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', {})
+    end,
+  }
 }
